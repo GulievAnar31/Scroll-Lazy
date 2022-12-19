549abd9fe29fb5215300311ec02962f629cc2d14
guliev <guliev@internet.ru>
2022-12-19 14:31:27 +0200

    fix

--- src/components/Photo/index.tsx
+++ src/components/Photo/index.tsx
@@ -5,12 +5,13 @@ import styles from './style.module.scss';
 export const Photo: React.FC<{ src: string }> = ({ src }) => {
     const { ref, inView } = useInView({
         threshold: 0.8,
-        triggerOnce: true,
     });
 
     React.useEffect(() => {
         console.log(inView);
     }, [inView]);
+
+    // так же можно добавтиь атрибут loadingLazy вместе trigersOne
 
     return <div ref={ref}>
         {inView ? <img className={styles.img} src={src} /> : <div className={styles.skeleton}></div>}
